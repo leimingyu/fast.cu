cef37735f4a6555f22b1c9981e61c2129d9f2078
Leiming Yu <leimingyu830@gmail.com>
2025-02-26 22:44:02 -0500

    up

--- matmul.cu
+++ matmul.cu
@@ -298,8 +298,8 @@ __global__ void matmul_fp8_64x8x32_kernel(
 	//uint64_t descA = make_smem_desc(sA, 32, 64);
 	//uint64_t descB = make_smem_desc(sB, 32,  8);
 
-	uint64_t descA = make_smem_desc(sA, 32, 64);
-	uint64_t descB = make_smem_desc(sB, 8,  32);
+	uint64_t descA = make_smem_desc(sA, 32, 32);
+	uint64_t descB = make_smem_desc(sB, 32, 32);
 
 
 	//-----------//
